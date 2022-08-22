d19dcddfa0cd15117deeec9057333c7875b548f3
Cristiano <crisbecker50@gmail.com>
2022-08-22 18:25:28 -0300

    mais exercicios

---
+++ exJS.js
@@ -0,0 +1,24 @@
+let n = 0;
+
+for (let index = 0; index <= 50; index += 1) {
+    n += index
+}
+
+console.log('A soma desses números é de '+ n +'!')
+
+
+
+
+
+
+let c = 0
+for (let index = 2; index <= 150; index += 1) {
+    if (index % 3 === 0) {
+        c += 1
+    }
+}
+
+if (c === 50) {
+    console.log('Metallica é foda!')
+}
+
