969802f3147aeaa13f9c759fcfd740f8d05c0bd4
Cristiano <crisbecker50@gmail.com>
2022-09-16 15:12:19 -0300

    finalizando bloco 6

---
+++ bloco6/form.js
@@ -0,0 +1,15 @@
+const apagar = document.getElementById('botoes2');
+
+function clean() {
+  const formElements = document.querySelectorAll('input');
+  const textArea = document.querySelector('textarea');
+  for (let index = 0; index < formElements.length; index += 1) {
+    const userInput = formElements[index];
+    userInput.value = '';
+    userInput.checked = false;
+  }
+  textArea.value = '';
+}
+
+
+apagar.addEventListener('click', clean)
